6aa2b599b1b06612370ba875d466b8dd882f8df9
MomoDog <wjx_ncepu@126.com>
2021-08-13 12:46:04 +0800

    Update star.py

--- star.py
+++ star.py
@@ -1,10 +1,4 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import seaborn as sns
-from time import time
-import glob
 
 def ListDim(alist):
     '''
